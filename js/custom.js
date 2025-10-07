098c9ba1177626ab639be5df40e0a329367042f5
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-10-07 23:40:35 +0530

    Comment out setNavHeight function and event listeners
    The setNavHeight function and its associated load and resize event listeners have been commented out, possibly for debugging or to temporarily disable dynamic nav height adjustment.

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 // Run on load and resize
-function setNavHeight() {
-    const nav = document.getElementById('nav');
-    const navHeight = nav.offsetHeight;
-    document.documentElement.style.setProperty('--nav-height', `${navHeight}px`);
-}
+// function setNavHeight() {
+//     const nav = document.getElementById('nav');
+//     const navHeight = nav.offsetHeight;
+//     document.documentElement.style.setProperty('--nav-height', `${navHeight}px`);
+// }
 
-window.addEventListener('load', setNavHeight);
-window.addEventListener('resize', setNavHeight);
+// window.addEventListener('load', setNavHeight);
+// window.addEventListener('resize', setNavHeight);
 
 // ====================================
 // Menu Tab Functionality
